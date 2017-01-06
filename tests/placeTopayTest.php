49db0b4755d99a0570bd3f2d83e626842b5f1154
System Administrator <root@iMac-de-Eduard.local>
2017-01-06 18:25:34 -0500

    first commit

---
+++ tests/placeTopayTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardrussy
+ * Date: 6/01/17
+ * Time: 17:40
+ */
+
+namespace bukerllk\placeToPay\tests;
+
+//use bukerllk\placeToPay\placeToPay;
+
+class placeToPayTest extends \PHPUnit_Framework_TestCase
+{
+    public function nothingTest()
+    {
+        $this->assertTre(true);
+    }
+
+}
